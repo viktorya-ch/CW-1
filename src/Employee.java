c9023719841e2061805502d0e8ed172f9489f078
origin <viktoryaa2000@mail.ru>
2024-12-03 00:34:05 +0300

    Commit1

---
+++ src/Employee.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int idCounter = 1;
+
+    private String fullName;
+    private int department;
+    private int salary;
+    private int id;
+
+    public Employee(String fullName, int department, int salary) {
+        id = idCounter++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return " fullName " + this.fullName + " department = " + this.department + " salary =" + this.salary + " id " + this.id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && salary == employee.salary && id == employee.id && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, department, salary, id);
+    }
+}
